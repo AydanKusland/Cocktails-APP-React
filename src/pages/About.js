bb5f53d17d37ccd39c5df55937706d2845cb6c1a
AydanKusland <chinatugov@mail.ru>
2023-05-06 19:26:04 +0800

    project finished

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+	return (
+		<section className='section about-section'>
+			<h1 className='section-title'>about us</h1>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore
+				necessitatibus rerum atque iusto animi facilis reprehenderit adipisci
+				corporis amet earum odit ipsa quidem ad quo deserunt qui, consequatur
+				eos dolor possimus voluptas illum? Ipsam aut eligendi reprehenderit,
+				totam doloribus officia.
+			</p>
+		</section>
+	)
+}
+
+export default About
